f7390575aefde28ae3260773aa77c366a543538b
Shawn Mclean <shawnrmclean@gmail.com>
2018-04-19 14:42:42 -0500

    Added some more test cases for access-on-weekdays-only-for-an-app

--- test/rules/access-on-weekdays-only-for-an-app.test.js
+++ test/rules/access-on-weekdays-only-for-an-app.test.js
@@ -1,4 +1,4 @@
-const loadRule = require('../utils/load-rule')
+const loadRule = require('../utils/load-rule');
 
 describe('access-on-weekdays-only-for-an-app', () => {
   let user;
@@ -9,18 +9,31 @@ describe('access-on-weekdays-only-for-an-app', () => {
     context = {
       clientName: 'TheAppToCheckAccessTo'
     };
-    global.UnauthorizedError = function() {}
+    global.UnauthorizedError = function() {};
 
-    rule = loadRule('access-on-weekdays-only-for-an-app.js')
+    rule = loadRule('access-on-weekdays-only-for-an-app.js');
   });
 
   describe('when day is weekend', () => {
     beforeEach(() => {
-      global.Date.getDay = jest.genMockFunction().mockReturnValue(6)
+      global.Date.getDay = jest.genMockFunction().mockReturnValue(6);
     })
     it('should return UnauthorizedError', (done) => {
       rule(user, context, (err, user, context) => {
         expect(err).toBeInstanceOf(global.UnauthorizedError);
+        done();
+      });
+    });
+  })
+  describe('when day is week day', () => {
+    beforeEach(() => {
+      global.Date.getDay = jest.genMockFunction().mockReturnValue(3);
+    })
+    it('should return no error', (done) => {
+      rule(user, context, (err, cbUser, cbContext) => {
+        expect(err).toBeNull();
+        expect(user).toBe(user);
+        expect(context).toBe(context);
         done();
       });
     });
