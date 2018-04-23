630ded6df62a7751d7d26beadec3336eb09fe25f
Shawn Mclean <shawnrmclean@gmail.com>
2018-04-23 16:37:45 -0500

    mixpanel-track-event migrated.

---
+++ src/rules/mixpanel-track-event.js
@@ -0,0 +1,34 @@
+/**
+ * @overview Tracks Logins in MixPanel
+ * @gallery true
+ * @category webhook
+ * @description Send a Sign In event to MixPanel to track logins
+ * 
+ * This rule will send a `Sign In` event to MixPanel, and will include the application the user is signing in to as a property. 
+ * See <a href="https://mixpanel.com/help/reference/http">MixPanel HTTP API</a> for more information.
+ * 
+ */
+
+function (user, context, callback) {
+
+  const mpEvent = {
+    "event": "Sign In",
+    "properties": {
+        "distinct_id": user.user_id,
+        "token": "{REPLACE_WITH_YOUR_MIXPANEL_TOKEN}",
+        "application": context.clientName
+    }
+  };
+
+  const base64Event = Buffer.from(JSON.stringify(mpEvent)).toString('base64');
+
+  request.get({
+    url: 'http://api.mixpanel.com/track/',
+    qs: {
+      data: base64Event
+    }
+  }, (err, res, body) => {
+      // don’t wait for the MixPanel API call to finish, return right away (the request will continue on the sandbox)`
+      callback(null, user, context);
+  });
+}
