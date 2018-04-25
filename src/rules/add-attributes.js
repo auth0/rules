a979bf53aaf112ee2b31313f1ab2ad54bb3928d3
Shawn Mclean <shawnrmclean@gmail.com>
2018-04-24 23:45:02 -0500

    add-attributes migrated

---
+++ src/rules/add-attributes.js
@@ -0,0 +1,18 @@
+/**
+ * @overview Add attributes to a user for specific connection. 
+ * @gallery true
+ * @category enrich profile
+ * @description Add attributes to a user for specific connection.
+ * 
+ * This rule will add an attribute to the user only for the login transaction (i.e. they won't be persisted to the user).
+ * This is useful for cases where you want to enrich the user information for a specific application.
+ * 
+ */
+
+function (user, context, callback) {
+  if (context.connection === 'company.com') {
+    context.idToken['https://example.com/vip'] = true;
+  }
+
+  callback(null, user, context);
+}
