3f94e8df9c59991b505621abe91ff32fc3fec8d4
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2018-10-01 12:52:21 +0300

    batch 11

---
+++ src/rules/remove-attributes.js
@@ -0,0 +1,25 @@
+/**
+ * @overview Remove attributes from a user
+ * @gallery true
+ * @category enrich profile
+ *
+ * Remove attributes from a user
+ *
+ * Sometimes you don't need every attribute from the user. You can use a rule to delete attributes.
+ */
+
+function (user, context, callback) {
+  delete user.some_attribute;
+
+  // another option would be to define a whitelist of attributes you want,
+  // instead of delete the ones you don't want
+  /*
+  var whitelist = ['email', 'name', 'identities'];
+  Object.keys(user).forEach(function(key) {
+    console.log(whitelist.indexOf(key));
+    if (whitelist.indexOf(key) === -1) delete user[key];
+  });
+  */
+
+  callback(null, user, context);
+}
