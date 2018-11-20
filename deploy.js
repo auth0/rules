80ae00fc5f4e13f8c234e9bcd18d4645833ef8ba
Alexander Zarubin <nonexist.void@gmail.com>
2018-11-20 13:25:08 +0200

    added deploy script for automatic upload the rules.json to the aws s3

---
+++ deploy.js
@@ -0,0 +1,38 @@
+const aws = require('aws-sdk');
+const rules = require('./rules.json');
+require('dotenv').config();
+
+(function () {
+  if (!process.env.S3_ACCESS_KEY || !process.env.S3_SECRET || !process.env.S3_REGION || !process.env.S3_BUCKET) {
+    console.log('AWS S3 settings missing. S3_ACCESS_KEY, S3_SECRET, S3_REGION and S3_BUCKET settings are required.');
+    process.exit();
+  }
+
+  console.log(`Deploying rules.json into ${process.env.S3_BUCKET} bucket...`);
+
+  const S3 = new aws.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET,
+    sessionToken: process.env.S3_SESSION_TOKEN,
+    region: process.env.S3_REGION,
+    params: {
+      Bucket: process.env.S3_BUCKET
+    }
+  });
+
+  const params = {
+    Body: JSON.stringify(rules),
+    Key: 'rules/rules.json',
+    ContentType: 'application/json'
+  };
+
+  return S3.putObject(params, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('rules.json deployed successfully.');
+    }
+
+    process.exit();
+  });
+})();
