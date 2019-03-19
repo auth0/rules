8a973635420edffad759a9db3f41b4675faa7294
joseeantonior <joseantonio.rey@auth0.com>
2019-03-19 13:18:45 -0500

    Fixing "Date is not defined"error (#200)
    * Update access-on-weekdays-only-for-an-app.js
    
    Fixing "Date is not defined"error
    
    * Fix test for `access-on-weekdays-only-for-an-app`

--- src/rules/access-on-weekdays-only-for-an-app.js
+++ src/rules/access-on-weekdays-only-for-an-app.js
@@ -11,7 +11,8 @@
 function (user, context, callback) {
 
   if (context.clientName === 'TheAppToCheckAccessTo') {
-    const d = Date.getDay();
+    const date = new Date();
+    const d = date.getDay();
 
     if (d === 0 || d === 6) {
       return callback(new UnauthorizedError('This app is available during the week'));
