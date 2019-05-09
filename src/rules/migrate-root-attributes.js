021c235a22edaf8f638890c0620e8255ab648131
Gustavo Jucoski Fernandes <gustavo.jucoski@auth0.com>
2019-05-09 14:36:47 -0300

    IUM-415: Enable the root attribute migration script

--- src/rules/migrate-root-attributes.js
+++ src/rules/migrate-root-attributes.js
@@ -11,7 +11,7 @@
  *
  * 1- The rule updates the profile root attribute with the mapped field from user_metadata.
  * 2- The mapped fields from user_metadata will be removed following the update.
- * 3- This rule will be executed on each login event. Consider using it only if you use a custom signup form or Authentication Signup API.
+ * 3- This rule will be executed on each login event. For signup scenarios, you should only consider using this rule if you currently use a custom signup form or Authentication Signup API, as these signup methods do not support setting the root attributes.
  */
 function (user, context, cb) {
   // Field Mapping, the property is the root attribute and the value is the field name on user_metadata.
