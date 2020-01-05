413a29cefcbaf0693428158d16d4f103e0be780c
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-01-04 23:47:30 -0800

    Update to FullContact API v3

--- src/rules/get-fullcontact-profile.js
+++ src/rules/get-fullcontact-profile.js
@@ -1,51 +1,56 @@
 /**
  * @title Enrich profile with FullContact
- * @overview Get the user profile from FullContact using the email then add a new property to user_metadata.
+ * @overview Get the user profile from FullContact using the email and user nickname then add a new property to user_metadata.
  * @gallery true
  * @category enrich profile
  *
- * This rule gets the user profile from FullContact using the e-mail (if available).
+ * This rule gets the user profile from FullContact using the e-mail and user nickname (if available).
  *
- * If the information is immediately available (signaled by a `statusCode=200`), it adds a new property `fullContactInfo` to the user_metadata and returns. Any other conditions are ignored.
+ * If the information is immediately available (signaled by a `statusCode=200`), it adds a new property `fullcontact` to the user_metadata and returns. Any other conditions are ignored.
  *
- * See [FullContact docs](http://www.fullcontact.com/developer/docs/) for full details.
+ * See [FullContact docs](https://dashboard.fullcontact.com/api-ref) for full details.
  *
  */
 
 function (user, context, callback) {
-  const FULLCONTACT_KEY = configuration.FULLCONTACT_KEY;
-  const SLACK_HOOK = configuration.SLACK_HOOK_URL;
+  // Get FullContact API key: https://dashboard.fullcontact.com/
+  const FULLCONTACT_KEY = 'YOUR FULLCONTACT API KEY'; 
+
+  // Create a Slack app and get a hook URL: https://api.slack.com/messaging/webhooks#getting-started
+  const SLACK_HOOK = 'YOUR SLACK HOOK URL';
 
   const slack = require('slack-notify')(SLACK_HOOK);
 
   // skip if no email
   if (!user.email) return callback(null, user, context);
 
-  // skip if fullcontact metadata is already there
+  // skip if FullContact metadata is already there
   if (user.user_metadata && user.user_metadata.fullcontact) return callback(null, user, context);
 
-  request.get('https://api.fullcontact.com/v2/person.json', {
-    qs: {
-      email:  user.email,
-      apiKey: FULLCONTACT_KEY
+  request.get('https://api.fullcontact.com/v3/person.enrich', {
+    'headers': {
+      'Authorization': 'Bearer ' + FULLCONTACT_KEY,
     },
-    json: true
+    body: JSON.stringify({
+      "email": user.email,
+      "twitter": `@${user.nickname}`
+    }),
   }, (error, response, body) => {
     if (error || (response && response.statusCode !== 200)) {
 
       slack.alert({
         channel: '#slack_channel',
-        text: 'Fullcontact API Error',
+        text: 'FullContact API Error',
         fields: {
           error: error ? error.toString() : (response ? response.statusCode + ' ' + body : '')
         }
       });
 
-      // swallow fullcontact api errors and just continue login
+      // swallow FullContact api errors and just continue login
       return callback(null, user, context);
     }
 
-    // if we reach here, it means fullcontact returned info and we'll add it to the metadata
+    // if we reach here, it means FullContact returned info and we'll add it to the metadata
     user.user_metadata = user.user_metadata || {};
     user.user_metadata.fullcontact = body;
 
