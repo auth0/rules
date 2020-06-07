16020490c3cbfc471dcff80f3668ed1112ac71f4
marygreenleeAUTH0 <mary.greenlee@auth0.com>
2020-06-07 13:44:51 -0700

    fix name on file, at Const to test file

--- test/rules/username-attribute.test.js
+++ test/rules/username-attribute.test.js
@@ -3,6 +3,7 @@
 const loadRule = require('../utils/load-rule');
 const ContextBuilder = require('../utils/contextBuilder');
 const RequestBuilder = require('../utils/requestBuilder');
+const UserBuilder = require ('../utils/userBuilder');
 
 const ruleName = 'username-attribute';
 
@@ -30,15 +31,12 @@ describe(ruleName, () => {
       rule = loadRule(ruleName, globals);
     });
   
-    it('should persist the attribute to user', (done) => {
+    it('should add username attribute to user', (done) => {
       const updateUserMetadataMock = globals.auth0.users.updateUserMetadata;
       updateUserMetadataMock.mockReturnValue(Promise.resolve());
       
       rule(user, context, (e, u, c) => {
-        const call = updateUserMetadataMock.mock.calls[0];
-        expect(call[0]).toBe(user.user_id);
-        expect(c.idToken['https://example.com/username']).toBe(user.app_metadata.username);
-        expect(call[1].username).toBe(user.app_metadata.username);
+        expect (user.app_metadata.username).toBe(user1);
         done();
       });
     });
