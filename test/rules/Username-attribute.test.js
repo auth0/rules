c6220278427fb45839cc2fc443228b400b17b2cf
marygreenleeAUTH0 <mary.greenlee@auth0.com>
2020-06-07 12:52:11 -0700

    update to username-attribute rule

--- test/rules/Username-attribute.test.js
+++ test/rules/Username-attribute.test.js
@@ -4,42 +4,42 @@ const loadRule = require('../utils/load-rule');
 const ContextBuilder = require('../utils/contextBuilder');
 const RequestBuilder = require('../utils/requestBuilder');
 
-const ruleName = 'Add Username to App MetaData';
+const ruleName = 'username-attribute';
 
 describe(ruleName, () => {
-  let rule;
-  let context;
-  let user;
-  let globals;
-
-  beforeEach(() => {
-    globals = {
-      global: {},
-      auth0: {
-        users: {
-          updateAppMetadata: jest.fn()
+    let rule;
+    let context;
+    let user;
+    let globals;
+  
+    beforeEach(() => {
+      globals = {
+        global: {},
+        auth0: {
+          users: {
+            updateUserMetadata: jest.fn()
+          }
         }
-      }
-    };
-
-    user = new UserBuilder()
-      .withAppMetadata({username: 'user1'})
-      .build();
-    context = new ContextBuilder().build();
-
-    rule = loadRule(ruleName, globals);
-  });
-
-  it('should persist the attribute to user', (done) => {
-    const updateAppMetadataMock = globals.auth0.users.updateAppMetadata;
-    updateAppMetadataMock.mockReturnValue(Promise.resolve());
-    
-    rule(user, context, (e, u, c) => {
-      const call = updateAppMetadataMock.mock.calls[0];
-      expect(call[0]).toBe(user.user_id);
-      expect(c.idToken['https://example.com/username']).toBe(user.app_metadata.username);
-      expect(call[1].username).toBe(user.app_metadata.username);
-      done();
+      };
+  
+      user = new UserBuilder()
+        .withAppMetadata({username: 'user1'})
+        .build();
+      context = new ContextBuilder().build();
+  
+      rule = loadRule(ruleName, globals);
+    });
+  
+    it('should persist the attribute to user', (done) => {
+      const updateUserMetadataMock = globals.auth0.users.updateUserMetadata;
+      updateUserMetadataMock.mockReturnValue(Promise.resolve());
+      
+      rule(user, context, (e, u, c) => {
+        const call = updateUserMetadataMock.mock.calls[0];
+        expect(call[0]).toBe(user.user_id);
+        expect(c.idToken['https://example.com/username']).toBe(user.app_metadata.username);
+        expect(call[1].username).toBe(user.app_metadata.username);
+        done();
+      });
     });
   });
-});
