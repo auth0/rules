cab6bbf6a0fba91f30ea14a13086be464f178d94
Josh Cunningham <josh@joshcanhelp.com>
2020-09-09 11:04:57 -0700

    First pass

---
+++ src/rules/onetrust-consent-management.js
@@ -0,0 +1,35 @@
+/**
+ * @title OneTrust Consent Management
+ * @overview Enhance Auth0 user profiles with consent, opt-ins and communication preferences data.
+ * @gallery true
+ * @category marketplace
+ */
+/* global configuration */
+async function oneTrustConsentManagement(user, context, callback) {
+  const { Auth0RedirectRuleUtilities } = require("@auth0/rule-utilities@0.1.0");
+  const axios = require("axios@0.19.2");
+
+  const {
+    ONETRUST_REQUEST_INFORMATION,
+    ONETRUST_CONSENT_API_URL,
+    ONETRUST_PURPOSE_ID,
+    ONETRUST_ADD_ID_TOKEN_CLAIM,
+  } = configuration;
+
+  const addIdTokenClaim = configuration.ONETRUST_ADD_ID_TOKEN_CLAIM === "true";
+
+  user.app_metadata = user.app_metadata || {};
+  user.app_metadata.onetrust = user.app_metadata.onetrust || {};
+
+  if (context.stats.loginsCount > 1 || ) {
+    return callback(null, user, context);
+  }
+
+  const response = await axios.post(ONETRUST_CONSENT_API_URL, {
+    identifier: user.email,
+    requestInformation: ONETRUST_REQUEST_INFORMATION,
+    purposes: [{ Id: ONETRUST_PURPOSE_ID }],
+  });
+
+  return callback(null, user, context);
+}
