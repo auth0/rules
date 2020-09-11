4870df8f243d4b708252b1b5b79954500252bd34
Josh Cunningham <josh@joshcanhelp.com>
2020-09-11 10:31:52 -0700

    Clean up config and add claims

--- src/rules/onetrust-consent-management.js
+++ src/rules/onetrust-consent-management.js
@@ -13,15 +13,16 @@ async function oneTrustConsentManagement(user, context, callback) {
     ONETRUST_REQUEST_INFORMATION,
     ONETRUST_CONSENT_API_URL,
     ONETRUST_PURPOSE_ID,
-    ONETRUST_ADD_ID_TOKEN_CLAIM,
   } = configuration;
 
   const addIdTokenClaim = configuration.ONETRUST_ADD_ID_TOKEN_CLAIM === "true";
+  const idTokenStatusClaim = "https://onetrust.com/status";
+  const idTokenLinkClaim = "https://onetrust.com/magic-link";
 
   user.app_metadata = user.app_metadata || {};
   user.app_metadata.onetrust = user.app_metadata.onetrust || {};
 
-  if (context.stats.loginsCount > 1 || ) {
+  if (context.stats.loginsCount > 1) {
     return callback(null, user, context);
   }
 
