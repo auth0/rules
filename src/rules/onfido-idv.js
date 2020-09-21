8a0017e2065556857e2eec1fbf01ee14d9ca5c4b
Shu Shen <shu.shen@auth0.com>
2020-09-21 16:15:47 -0700

    fix: onfido missing trailing semicolon

--- src/rules/onfido-idv.js
+++ src/rules/onfido-idv.js
@@ -49,7 +49,7 @@ async function onfidoIdentityVerification(user, context, callback) {
   });
 
   user.app_metadata = user.app_metadata || {};
-  user.app_metadata.onfido = user.app_metadata.onfido || {}
+  user.app_metadata.onfido = user.app_metadata.onfido || {};
 
   if (ruleUtils.isRedirectCallback && ruleUtils.queryParams.session_token && "true" === ruleUtils.queryParams.onfido_idv) {
     // User is back from the Onfido experience and has a session token to validate and assign to user meta
