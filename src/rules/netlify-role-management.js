528664559edd25f02b9e3805c6d54958d130b914
James Quick <james.q.quick@outlook.com>
2020-12-09 08:28:04 -0600

    Added formatting

--- src/rules/netlify-role-management.js
+++ src/rules/netlify-role-management.js
@@ -11,32 +11,36 @@
  *    - `CUSTOM_CLAIMS_NAMESPACE` - namespace for adding custom claims to ID Token
  */
 
-async function (user, context, callback) {
-
+async function netlifyRoleManagement(user, context, callback) {
   const ManagementClient = require('auth0@2.27.1').ManagementClient;
-  
-  const namespace = configuration.CUSTOM_CLAIMS_NAMESPACE || "https://netlify-integration.com";
+
+  const namespace =
+    configuration.CUSTOM_CLAIMS_NAMESPACE || 'https://netlify-integration.com';
   const assignedRoles = (context.authorization || {}).roles;
   const defaultRoleName = configuration.DEFAULT_ROLE_NAME;
   const defaultRoleId = configuration.DEFAULT_ROLE_ID;
-  
+
   //give default role if the user doesn't already have any roles assigned
-  if( (!assignedRoles || assignedRoles.length === 0) && defaultRoleName && defaultRoleId) {
-  	try {
-        const management = new ManagementClient({
-          token: auth0.accessToken,
-          domain: auth0.domain
-        });
-        await management.assignRolestoUser(
-          { id : user.user_id}, 
-          { "roles" :[defaultRoleId]}
-        );
-      	assignedRoles.push(defaultRoleName);
-    }catch(ex){
-      console.error("Failed to add default role to user");
+  if (
+    (!assignedRoles || assignedRoles.length === 0) &&
+    defaultRoleName &&
+    defaultRoleId
+  ) {
+    try {
+      const management = new ManagementClient({
+        token: auth0.accessToken,
+        domain: auth0.domain
+      });
+      await management.assignRolestoUser(
+        { id: user.user_id },
+        { roles: [defaultRoleId] }
+      );
+      assignedRoles.push(defaultRoleName);
+    } catch (ex) {
+      console.error('Failed to add default role to user');
     }
   }
-  
-  context.idToken[namespace + "/roles"] = assignedRoles;
-  return callback(null, user, context);  
+
+  context.idToken[namespace + '/roles'] = assignedRoles;
+  return callback(null, user, context);
 }
