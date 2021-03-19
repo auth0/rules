d8d39dd5323a9bb66ef0ccf068676bbb9ead405b
thook-sz <thook@seczetta.com>
2021-03-19 12:45:01 -0500

    Updated to include error checking when a user isnt found in SecZett

--- src/rules/seczetta-grab-risk-score.js
+++ src/rules/seczetta-grab-risk-score.js
@@ -1,7 +1,7 @@
 /* global configuration, auth0 */
 
 /**
- * @title Get SecZetta Risk Score
+ * @title seczettaGrabRiskScore
  * @overview Grab the risk score from SecZetta to use in the authentication flow
  * @gallery true
  * @category multifactor"
@@ -11,8 +11,6 @@
  *
  *    - `SECZETTA_API_KEY` API Token from your SecZetta tennant
  *    - `SECZETTA_BASE_URL` URL for your SecZetta tennant
- *    
- *  Required Config that isnt setup yet, these IDs are used in the advanced search
  *    - `SECZETTA_ATTRIBUTE_ID` the id of the SecZetta attribute you are searching on (i.e personal_email, user_name, etc.)
  *    - `SECZETTA_PROFILE_TYPE_ID' the id of the profile type this user's profile
  *
@@ -30,39 +28,16 @@
  * @param {object} context
  * @param {function} callback
  */
-async function getRiskScoreFromApiExample(user, context, callback) {
+async function seczettaGrabRiskScore(user, context, callback) {
   
   if (!configuration.SECZETTA_API_KEY || !configuration.SECZETTA_BASE_URL) {
     console.log("Missing required configuration. Skipping.");
     return callback(null, user, context);
   }
 
-  // OPTIONAL: check the Application metadata to see if this API should be skipped.
-  // TH: removed this for now, kept gettin gerrors
-  /*if (context.clientMetadata.getUserDataFromApi === "false") {
-    return callback(null, user, context);
-  }*/
-
-  // TODO: Update with integration-specific namespace.
   const userUtils = new require(
     "@auth0/rule-utilities@0.2.0"
-  );//.Auth0UserUpdateUtilities(user, auth0, "namespace");
-  //TH: Removed the update utilities. Giving error Auth0UserUpdateUtililes requires 'new' 
-  //Auth0UserUpdateUtilities(user, auth0, "namespace");
-
-  // Build the user object to POST to the API.
-  // This call is M2M so we're not worried about tampering or PII.
-  // TH: NOT USING RIGHT NOW. Need to understand the best way to pull the risk data from SecZetta 
-  // const userData = {
-  //     // https://auth0.com/docs/rules/context-object
-  //     ip: context.request.ip,
-  //     userAgent: context.request.userAgent,
-
-  //     // https://auth0.com/docs/rules/user-object-in-rules
-  //     user_id: user.user_id,
-  //     email: user.email,
-  //     email_verified: !!user.email_verified,
-  // };
+  );
 
   const axios = require("axios@0.19.2");
       
@@ -130,9 +105,11 @@ async function getRiskScoreFromApiExample(user, context, callback) {
   //  >>    ]
   //  >> }
 
-
-  let profileReponse;
+  let profileResponse;
   let riskScoreResponse;
+
+  let attributeId = configuration.SECZETTA_ATTRIBUTE_ID;
+  let profileTypeId = configuration.SECZETTA_PROFILE_TYPE_ID;
 
   let uid = user.user_name || user.email; //depends on the configuration
   let profileRequestUrl = configuration.SECZETTA_BASE_URL + '/advanced_search/run';
@@ -143,11 +120,11 @@ async function getRiskScoreFromApiExample(user, context, callback) {
         {
           "type": "ProfileTypeRule",
           "comparison_operator": "==",
-          "value": "efa0e8e1-a193-4596-9081-ccf4ea9d0c07"
+          "value": profileTypeId
         },
         {
           "type": "ProfileAttributeRule",
-          "condition_object_id": "7cffa07d-ad6d-4398-ba07-b3d1e5f9ee9f",
+          "condition_object_id": attributeId,
           "object_type": "NeAttribute",
           "comparison_operator": "==",
           "value": uid
@@ -157,26 +134,36 @@ async function getRiskScoreFromApiExample(user, context, callback) {
   };
   
   try {
-      profileReponse = await axios.post(profileRequestUrl,advancedSearchBody,{
-      headers: {
-        'Content-Type':'application/json',
-        'Authorization': 'Token token='+configuration.SECZETTA_API_KEY,
-        'Accept': 'application/json'
-      },
-    });
+    	profileResponse = await axios.post(profileRequestUrl,advancedSearchBody,{
+        headers: {
+          'Content-Type':'application/json',
+          'Authorization': 'Token token='+configuration.SECZETTA_API_KEY,
+          'Accept': 'application/json'
+        },
+      });
+
+      //if the user isnt found via the advanced search. A 
+    	if( profileResponse.data.profiles.length === 0 ) {
+        console.log("Profile not found. Empty Array sent back!");
+  	 		if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
+          return callback(null, user, context);
+        }
+        return callback(new UnauthorizedError("Error retrieving Risk Score."));
+  		}
+    
   } catch (profileError) {
     // Swallow risk scope API call, default is set to highest risk below.
     console.log(`Error while calling Profile API: ${profileError.message}`);
     if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
       return callback(null, user, context);
     }
-    return callback(new UnauthorizedError("Error retreviing Risk Score."));
+    return callback(new UnauthorizedError("Error retrieving Risk Score."));
   }
 
   //Should now have the profile in profileResponse. Lets grab it.
-  //TODO: error checks to make sure its an array of 1
-  //TODO: What happens when user doesnt exist?
-  let objectId = profileReponse.data.profiles[0].id;
+  let objectId = profileResponse.data.profiles[0].id;
+  console.log(objectId);
+
   let riskScoreRequestUrl = configuration.SECZETTA_BASE_URL + '/risk_scores?object_id=' + objectId;
 
   try {
@@ -193,44 +180,20 @@ async function getRiskScoreFromApiExample(user, context, callback) {
     if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
       return callback(null, user, context);
     }
-    return callback(new UnauthorizedError("Error retreviing Risk Score."));
+    return callback(new UnauthorizedError("Error retrieving Risk Score."));
   }
 
   //Should now finally have the risk score. Lets add it to the user
-  //TODO: Too many chances for failure. Stressful..
   var riskScoreObj = riskScoreResponse.data.risk_scores[0];
   var overallScore = riskScoreObj.overall_score;
-
-  if(DEBUG) console.log("Risk Score: " + overallScore);
 
   // Default risk value is set to highest if API fails or no score returned.
   //var riskScore = typeof apiResponse.riskScore === "number" ? riskScore : 100;
 
-  // TODO: Update with integration-specific namespace.
-  configuration.NAMESPACE_SAVE_TO_USER = "yes";
-  if (configuration.NAMESPACE_SAVE_TO_USER === "yes") {
-    //userUtils.setAppMeta("risk_score", overallScore); not a function
-    user.app_metadata = { risk_score: overallScore };
-    console.log(user);
-    try {
-      //await userUtils.updateAppMeta();
-      //await auth0.users.updateAppMetadata(user.user_id, user.app_metadata);
-    } catch (error) {
-      // Swallow Auth0 error, login can continue.
-      console.log(`Error updating user: ${error.message}`);
-    }
-  }
-
-  // TODO: Adjust for real values and update with integration-specific namespace.
-  if (maximumRisk && overallScore > maximumRisk) {
-    console.log(
-      `Risk score ${overallScore} is greater than maximum of ${maximumRisk}`
-    );
-    return callback(new UnauthorizedError("A "+overallScore+" Risk score is too high. Maximum acceptable risk is " + maximumRisk));
-  }
-  // TODO: Adjust for real values and update with integration-specific namespace.
   const allowableRisk = parseInt(configuration.SECZETTA_ALLOWABLE_RISK, 10);
   const maximumRisk = parseInt(configuration.SECZETTA_MAXIMUM_ALLOWED_RISK, 10);
+  
+  //if risk score is below the maxium risk score but above allowable risk: Require MFA
   if (allowableRisk && overallScore > allowableRisk && overallScore < maximumRisk) {
     console.log(
       `Risk score ${overallScore} is greater than maximum of ${allowableRisk}. Prompting for MFA`
@@ -243,9 +206,10 @@ async function getRiskScoreFromApiExample(user, context, callback) {
   
   }
 
+  //if risk score is above the maxium risk score: Fail authN
   if (maximumRisk && overallScore > maximumRisk) {
     console.log(
-      `Risk score ${overallScore} is greater than maximum of 7`
+      `Risk score ${overallScore} is greater than maximum of ${maximumRisk}`
     );
     return callback(new UnauthorizedError("A "+overallScore+" Risk score is too high. Maximum acceptable risk is " + maximumRisk));
   }
