f6df6254ad2d37d2f39fce926035ad42e7963f5a
Jacobo Vidal <jacobo.vidal@arengu.com>
2021-03-24 15:20:51 +0100

    Add Arengu progressive profiling rule

---
+++ src/rules/arengu-progressive-profiling.js
@@ -0,0 +1,76 @@
+/**
+ * @title Arengu Progressive Profiling 
+ * @overview Capture new users' information in your authentication flows.
+ * @gallery true
+ * @category marketplace
+ */
+
+ async function arenguCompleteUserProfile(user, context, callback) {
+  if (
+    !configuration.SESSION_TOKEN_SECRET ||
+    !configuration.ARENGU_POLICIES_FORM_URL
+  ) {
+    console.log('Missing required configuration. Skipping.');
+    return callback(null, user, context);
+  }
+
+  const {
+    Auth0RedirectRuleUtilities,
+    Auth0UserUpdateUtilities
+  } = require("@auth0/rule-utilities@0.2.0");
+
+  const ruleUtils = new Auth0RedirectRuleUtilities(
+    user,
+    context,
+    configuration
+  );
+
+  const userUtils = new Auth0UserUpdateUtilities(user, auth0);
+
+  function validateSessionToken() {
+    try {
+      return ruleUtils.validateSessionToken();
+    } catch (error) {
+      callback(error);
+    }
+  }
+
+  // Modify your login criteria to your needs
+  function isLogin() {
+    return context.stats.loginsCount > 2;
+  }
+
+  function isEmptyUserMeta(key) {
+    return userUtils.getUserMeta(key) === undefined ||
+      userUtils.getUserMeta(key) === null ||
+      userUtils.getUserMeta(key).length === 0;
+  }
+
+  function isProfileIncomplete() {
+    // Add your required user_medata keys
+    return isEmptyUserMeta('job_title') || isEmptyUserMeta('company_name');
+  }
+
+  if (ruleUtils.isRedirectCallback && ruleUtils.queryParams.session_token) {
+    const decodedToken = validateSessionToken();
+    const customClaims = decodedToken.other;
+
+    for (const [key, value] of Object.entries(customClaims)) {
+      userUtils.setUserMeta(key, value);
+    }
+
+    try {
+      await userUtils.updateUserMeta();
+
+      return callback(null, user, context);
+    } catch (error) {
+      return callback(error);
+    }
+  }
+
+  if (isLogin() && isProfileIncomplete()) {
+    ruleUtils.doRedirect(configuration.ARENGU_PROFILE_FORM_URL);
+  }
+
+  return callback(null, user, context);
+}
