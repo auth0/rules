6d7c6ad6569593374840f46d0ca9b3f0208ceef0
Mateus Moury <mateus@inloco.com.br>
2021-03-26 14:48:46 -0300

    refactor(incognia-authentication.js): adding missing semi-colons and removing unused dep

--- src/rules/incognia-authentication.js
+++ src/rules/incognia-authentication.js
@@ -6,10 +6,9 @@
  *
  */
 async function incogniaAuthenticationRule(user, context, callback) {
-  const _ = require('lodash@4.17.19')
+  const _ = require('lodash@4.17.19');
 
-  const { IncogniaAPI } = require('@incognia/api@1.0.0')
-  const { Auth0UserUpdateUtilities } = require('@auth0/rule-utilities@0.2.0')
+  const { IncogniaAPI } = require('@incognia/api@1.0.0');
 
   const { INCOGNIA_CLIENT_ID, INCOGNIA_CLIENT_SECRET } = configuration;
 
