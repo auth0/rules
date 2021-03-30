14b12977eb05573a88026df191b8fd4792faeb15
Josh Cunningham <josh@joshcanhelp.com>
2021-03-30 15:43:48 -0700

    npm run format

--- src/rules/seczetta-grab-risk-score.js
+++ src/rules/seczetta-grab-risk-score.js
@@ -23,18 +23,22 @@
  *    - The SecZetta API documentation is located here: https://{{SECZETTA_BASE_URL}}/api/v1/
  */
 async function seczettaGrabRiskScore(user, context, callback) {
-
-  if (!configuration.SECZETTA_API_KEY || !configuration.SECZETTA_BASE_URL || !configuration.SECZETTA_ATTRIBUTE_ID || !configuration.SECZETTA_PROFILE_TYPE_ID || !configuration.SECZETTA_ALLOWABLE_RISK || !configuration.SECZETTA_MAXIMUM_ALLOWED_RISK) {
-    console.log("Missing required configuration. Skipping.");
+  if (
+    !configuration.SECZETTA_API_KEY ||
+    !configuration.SECZETTA_BASE_URL ||
+    !configuration.SECZETTA_ATTRIBUTE_ID ||
+    !configuration.SECZETTA_PROFILE_TYPE_ID ||
+    !configuration.SECZETTA_ALLOWABLE_RISK ||
+    !configuration.SECZETTA_MAXIMUM_ALLOWED_RISK
+  ) {
+    console.log('Missing required configuration. Skipping.');
     return callback(null, user, context);
   }
 
-  const userUtils = new require(
-    "@auth0/rule-utilities@0.2.0"
-  );
+  const userUtils = new require('@auth0/rule-utilities@0.2.0');
 
-  const axios = require("axios@0.21.1");
-  const URL = require("url").URL;
+  const axios = require('axios@0.21.1');
+  const URL = require('url').URL;
 
   let profileResponse;
   let riskScoreResponse;
@@ -43,76 +47,94 @@ async function seczettaGrabRiskScore(user, context, callback) {
   let profileTypeId = configuration.SECZETTA_PROFILE_TYPE_ID;
 
   let uid = user.username || user.email; //depends on the configuration
-  const profileRequestUrl = new URL('/api/advanced_search/run', configuration.SECZETTA_BASE_URL);
+  const profileRequestUrl = new URL(
+    '/api/advanced_search/run',
+    configuration.SECZETTA_BASE_URL
+  );
 
   let advancedSearchBody = {
     advanced_search: {
-      label: "All Contractors",
+      label: 'All Contractors',
       condition_rules_attributes: [
         {
-          "type": "ProfileTypeRule",
-          "comparison_operator": "==",
-          "value": profileTypeId
+          type: 'ProfileTypeRule',
+          comparison_operator: '==',
+          value: profileTypeId
         },
         {
-          "type": "ProfileAttributeRule",
-          "condition_object_id": attributeId,
-          "object_type": "NeAttribute",
-          "comparison_operator": "==",
-          "value": uid
+          type: 'ProfileAttributeRule',
+          condition_object_id: attributeId,
+          object_type: 'NeAttribute',
+          comparison_operator: '==',
+          value: uid
         }
       ]
     }
   };
 
   try {
-    	profileResponse = await axios.post(profileRequestUrl.href,advancedSearchBody,{
+    profileResponse = await axios.post(
+      profileRequestUrl.href,
+      advancedSearchBody,
+      {
         headers: {
-          'Content-Type':'application/json',
-          'Authorization': 'Token token='+configuration.SECZETTA_API_KEY,
-          'Accept': 'application/json'
-        },
-      });
-
-      //if the user isnt found via the advanced search. A
-    	if( profileResponse.data.profiles.length === 0 ) {
-        console.log("Profile not found. Empty Array sent back!");
-  	 		if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
-          return callback(null, user, context);
+          'Content-Type': 'application/json',
+          Authorization: 'Token token=' + configuration.SECZETTA_API_KEY,
+          Accept: 'application/json'
         }
-        return callback(new UnauthorizedError("Error retrieving Risk Score."));
-  		}
+      }
+    );
 
+    //if the user isnt found via the advanced search. A
+    if (profileResponse.data.profiles.length === 0) {
+      console.log('Profile not found. Empty Array sent back!');
+      if (
+        configuration.SECZETTA_AUTHENTICATE_ON_ERROR &&
+        configuration.SECZETTA_AUTHENTICATE_ON_ERROR === 'true'
+      ) {
+        return callback(null, user, context);
+      }
+      return callback(new UnauthorizedError('Error retrieving Risk Score.'));
+    }
   } catch (profileError) {
     // Swallow risk scope API call, default is set to highest risk below.
     console.log(`Error while calling Profile API: ${profileError.message}`);
-    if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
+    if (
+      configuration.SECZETTA_AUTHENTICATE_ON_ERROR &&
+      configuration.SECZETTA_AUTHENTICATE_ON_ERROR === 'true'
+    ) {
       return callback(null, user, context);
     }
-    return callback(new UnauthorizedError("Error retrieving Risk Score."));
+    return callback(new UnauthorizedError('Error retrieving Risk Score.'));
   }
 
   //Should now have the profile in profileResponse. Lets grab it.
   let objectId = profileResponse.data.profiles[0].id;
   console.log(objectId);
 
-  const riskScoreRequestUrl = new URL('/api/risk_scores?object_id=' + objectId, configuration.SECZETTA_BASE_URL);
+  const riskScoreRequestUrl = new URL(
+    '/api/risk_scores?object_id=' + objectId,
+    configuration.SECZETTA_BASE_URL
+  );
 
   try {
-      riskScoreResponse = await axios.get(riskScoreRequestUrl.href,{
+    riskScoreResponse = await axios.get(riskScoreRequestUrl.href, {
       headers: {
-        'Content-Type':'application/json',
-        'Authorization': 'Token token='+configuration.SECZETTA_API_KEY,
-        'Accept': 'application/json'
-      },
+        'Content-Type': 'application/json',
+        Authorization: 'Token token=' + configuration.SECZETTA_API_KEY,
+        Accept: 'application/json'
+      }
     });
   } catch (riskError) {
     // Swallow risk scope API call, default is set to highest risk below.
     console.log(`Error while calling Risk Score API: ${riskError.message}`);
-    if( configuration.SECZETTA_AUTHENTICATE_ON_ERROR && configuration.SECZETTA_AUTHENTICATE_ON_ERROR === "true" ) {
+    if (
+      configuration.SECZETTA_AUTHENTICATE_ON_ERROR &&
+      configuration.SECZETTA_AUTHENTICATE_ON_ERROR === 'true'
+    ) {
       return callback(null, user, context);
     }
-    return callback(new UnauthorizedError("Error retrieving Risk Score."));
+    return callback(new UnauthorizedError('Error retrieving Risk Score.'));
   }
 
   //Should now finally have the risk score. Lets add it to the user
@@ -126,7 +148,12 @@ async function seczettaGrabRiskScore(user, context, callback) {
   const maximumRisk = parseInt(configuration.SECZETTA_MAXIMUM_ALLOWED_RISK, 10);
 
   //if risk score is below the maxium risk score but above allowable risk: Require MFA
-  if ((allowableRisk && overallScore > allowableRisk && overallScore < maximumRisk) || (allowableRisk === 0)) {
+  if (
+    (allowableRisk &&
+      overallScore > allowableRisk &&
+      overallScore < maximumRisk) ||
+    allowableRisk === 0
+  ) {
     console.log(
       `Risk score ${overallScore} is greater than maximum of ${allowableRisk}. Prompting for MFA`
     );
@@ -135,7 +162,6 @@ async function seczettaGrabRiskScore(user, context, callback) {
       allowRememberBrowser: false
     };
     return callback(null, user, context);
-
   }
 
   //if risk score is above the maxium risk score: Fail authN
@@ -143,7 +169,14 @@ async function seczettaGrabRiskScore(user, context, callback) {
     console.log(
       `Risk score ${overallScore} is greater than maximum of ${maximumRisk}`
     );
-    return callback(new UnauthorizedError("A "+overallScore+" Risk score is too high. Maximum acceptable risk is " + maximumRisk));
+    return callback(
+      new UnauthorizedError(
+        'A ' +
+          overallScore +
+          ' Risk score is too high. Maximum acceptable risk is ' +
+          maximumRisk
+      )
+    );
   }
 
   if (configuration.SECZETTA_RISK_KEY) {
