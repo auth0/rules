b9abc3b968982bdf456f1da88d6f71d3d5338db5
Tuana Celik <tuana@cumul.io>
2021-07-20 17:42:21 +0100

    Cumul.io Rule
    Adding the rule here for the marketplace integration. Adds user_metadata.cumulio to id and access tokens to be used within cumulio. 
    I've also added a warning as to this is what the integration does to the installation guideline markdown file.

---
+++ src/rules/add-metadata-to-token.js
@@ -0,0 +1,18 @@
+/**
+ * @title User metadata for Cumul.io
+ * @overview Add Cumul.io user metaadata to token to be used for Cumul.io dashboard filtering
+ * @gallery true
+ * @category marketplace
+ */
+
+
+function addMetadataToToken(user, context, callback) {
+  const namespace = 'https://cumulio/';
+  user.user_metadata = user.user_metadata || {};
+  user.user_metadata.cumulio = user.user_metadata.cumulio || {};
+  Object.keys(user.user_metadata.cumulio).forEach((k) => {
+    context.idToken[namespace + k] = user.user_metadata.cumulio[k];
+    context.accessToken[namespace + k] = user.user_metadata.cumulio[k];
+  });
+  callback(null, user, context);
+}
