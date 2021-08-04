16d75cffcb86c94fd36089d37038159055ef70f5
Josh Cunningham <josh.cunningham@auth0.com>
2021-08-04 09:11:12 -0700

    Release 0.23.0 (#297)
    * version bump
    
    * npm run format
    
    * npm run release
    
    * move cumul.io rule
    
    * rebuild rules.json to add cumul.io rule

--- src/rules/cumulio-add-metadata-to-tokens.js
+++ src/rules/cumulio-add-metadata-to-tokens.js
@@ -4,24 +4,24 @@
  * @gallery true
  * @category marketplace
  *
- * This integration simplifies the process of making full use of integrated Cumul.io dashboards' multi tenant features 
- * by using Auth0 as its authentication layer. The integration will allow you to set up and use user 
+ * This integration simplifies the process of making full use of integrated Cumul.io dashboards' multi tenant features
+ * by using Auth0 as its authentication layer. The integration will allow you to set up and use user
  * information in Auth0 to filter and structure your Cumul.io dashboards.
  */
-
 
 function addMetadataToTokens(user, context, callback) {
   const namespace = 'https://cumulio/';
   user.user_metadata = user.user_metadata || {};
   const cumulioMetadata = user.user_metadata.cumulio || {};
-  if(typeof cumulioMetadata === 'object' && cumulioMetadata !== null){ 
+  if (typeof cumulioMetadata === 'object' && cumulioMetadata !== null) {
     Object.keys(cumulioMetadata).forEach((k) => {
       context.idToken[namespace + k] = cumulioMetadata[k];
       context.accessToken[namespace + k] = cumulioMetadata[k];
     });
-  }
-  else{
-  	console.log("Make sure that user_metadata.cumulio is an object with keys and values");
+  } else {
+    console.log(
+      'Make sure that user_metadata.cumulio is an object with keys and values'
+    );
     return;
   }
   callback(null, user, context);
