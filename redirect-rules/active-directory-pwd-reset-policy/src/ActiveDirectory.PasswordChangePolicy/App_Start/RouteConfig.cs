930c177c3248f385c7ccc98790bbd3db8ea0b138
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-06-29 03:08:18 +0600

    Update RouteConfig.cs

--- redirect-rules/active-directory-pwd-reset-policy/src/ActiveDirectory.PasswordChangePolicy/App_Start/RouteConfig.cs
+++ redirect-rules/active-directory-pwd-reset-policy/src/ActiveDirectory.PasswordChangePolicy/App_Start/RouteConfig.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
 
-namespace ActiveDirectory.PasswordChangePolicy
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapMvcAttributeRoutes();
-        }
-    }
-}
