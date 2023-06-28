fe45d11fa7c50f7b6dc8bd4351f7895c1c421d64
Jose pedro hurtado vega <josepedro06agosto@gmail.com>
2023-06-29 03:07:49 +0600

    Update FilterConfig.cs

--- redirect-rules/active-directory-pwd-reset-policy/src/ActiveDirectory.PasswordChangePolicy/App_Start/FilterConfig.cs
+++ redirect-rules/active-directory-pwd-reset-policy/src/ActiveDirectory.PasswordChangePolicy/App_Start/FilterConfig.cs
@@ -1,13 +1,1 @@
-﻿using System.Web;
-using System.Web.Mvc;
 
-namespace ActiveDirectory.PasswordChangePolicy
-{
-    public class FilterConfig
-    {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-    }
-}
